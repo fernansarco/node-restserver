058bb135962492c999160d5f16158d392a10db01
LuisFernando <fernansarco30@gmail.com>
2019-10-04 15:05:05 -0500

    RestServer - usuario

---
+++ server/models/usuario.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+let rolesValidos = {
+    values: ['ADMIN_ROLE', 'USER_ROLE'],
+    message: '{VALUE} no es un rol valido'
+};
+
+
+let usuarioSchema = new Schema({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre del usuario es obligatorio']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'El Email del usuario es obligatorio']
+    },
+    password: {
+        type: String,
+        required: [true, 'El password del usuario es obligatorio']
+    },
+    img: {
+        type: String,
+        required: false
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: rolesValidos
+    },
+    estado: {
+        type: Boolean,
+        default: true,
+        required: [true, 'El estado del usuario es obligatorio']
+    },
+    google: {
+        type: Boolean,
+        default: false,
+    },
+
+});
+//función para no mostrar el password por patalla
+usuarioSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+    return userObject;
+}
+
+
+usuarioSchema.plugin(uniqueValidator, { message: '{PATH} debe ser unico' });
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -8,40 +9,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
     // parse application/json
 app.use(bodyParser.json())
 
-//get para consultar
-app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
-})
+app.use(require('./routes/usuario'));
 
-//post para agregar datos
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'Se nececita el nombre de la persoan'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-})
+mongoose.connect(process.env.URLDB,
 
-//put para actualizar datos
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
+    { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
 
-//put para actualizar el estado de los datos
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-})
+
+        if (err) throw new err;
+
+        console.log('Base de datos a arriba');
+
+    });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto', process.env.PORT);
